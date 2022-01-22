2397affa6ba2bdf97b3a3319503ed89c790a9641
Satish Kumar <sksatishk9@gmail.com>
2022-01-22 12:13:05 +0530

    Day_one og JS

---
+++ Day1/hello.js
@@ -0,0 +1,5 @@
+//This is 1st comment
+//console.log()will print the string or anything inside the console.
+// sortcut (ctrl +/)
+console.log("hello World!");
+console.log(`Hello Satish, How are u doing!`);
---
+++ Day1/stringIndexing.js
@@ -0,0 +1,4 @@
+let myName = "Satish Kumar";
+console.log(myName[3]);
+const len = myName.length;
+console.log(len);
